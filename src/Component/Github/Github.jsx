9e88b65a1809019d76c0f7bc1a27978f4f65032d
agusTrih <ant3dong@gmail.com>
2020-07-06 20:13:01 +0700

    tes

--- src/Component/Github/Github.jsx
+++ src/Component/Github/Github.jsx
@@ -1,47 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 function Github() {
-    const [input, setInput] = useState();
-    const [datas, setDatas] = useState([]);
-    const fetchData = async () => {
-        const url = "https://api.github.com/users";
-        const response = await fetch(url);
-        const result = await response.json();
-        setDatas(result);
-        console.log(result);
+    // state
+    const [input, setInput] = useState("");
+
+    // function
+    const handleChange = (event) => {
+        setInput(event.target.value);
     };
-    useEffect(() => {
-        fetchData();
-    }, []);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
 
-    // const handleKeyUp = (event) => {
-    //     if (event.keyCode === 13) {
-    //         event.preventDefault();
-    //         setInput();
-    //         console.log(input);
-    //     }
-    // };
     return (
-        <div>
-            <input type="text" value={input} />
-
-            {datas.map((item) => {
-                return (
-                    <div key={item.login}>
-                        <p>{item.login}</p>
-                        <img src={item.avatar_url} alt="avatar" />
-                        <p>{item.bio}</p>
-                        <div>
-                            <p>{item.follower_url}</p>
-                            <p>{item.repos_url}</p>
-                            <Link to={`/github-detail/${item.login}`}>
-                                Detail
-                            </Link>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input onChange={handleChange} value={input} />
+            <button>
+                {" "}
+                <Link to={`/github-detail/${input}`}>klik</Link>
+            </button>
+        </form>
     );
 }
 
